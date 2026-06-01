54ebfcdff394886f86e29cbe733c5c71e9c4427d
rew <rew@tencorvids.com>
2026-06-01 19:43:34 -0400

    create ship-pve3 host dir

---
+++ hosts/ship-pve3/default.nix
@@ -0,0 +1,34 @@
+{
+  inputs,
+  self,
+  ...
+}:
+{
+  flake.nixosConfigurations.ship_pve3 = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = {
+      inherit inputs;
+      vars = self.vars.user;
+    };
+    modules = with inputs.self.modules.nixos; [
+      base
+      grub_boot
+      kernel
+      qemu_guest
+      virt
+      inputs.home-manager.nixosModules.default
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit inputs;
+          vars = self.vars.user;
+        };
+      }
+      ./_hardware-configuration.nix
+      ./_home.nix
+      ./_network.nix
+      ./_storage.nix
+    ];
+  };
+}
