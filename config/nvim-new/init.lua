1a494912f06449ee70f5b99b112219879d8e2986
rew <rew@tencorvids.com>
2026-07-11 13:22:37 -0400

    add mini and gitsigns

--- config/nvim-new/init.lua
+++ config/nvim-new/init.lua
@@ -1,11 +1,17 @@
 require("options")
 require("keymaps")
 
--- Plugins
+-- General
 vim.pack.add({
 	{ src = "https://github.com/scottmckendry/cyberdream.nvim", version = "v5.5.0" },
 	{ src = "https://github.com/stevearc/oil.nvim", version = "v2.16.0" },
 	{ src = "https://github.com/folke/snacks.nvim", version = "v2.31.0" },
+	{ src = "https://github.com/echasnovski/mini.nvim", version = "v0.18.0" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim", version = "v2.1.0" },
+})
+
+-- LSP & Co.
+vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/neovim/nvim-lspconfig", version = "v2.10.0" },
 	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
--- config/nvim-new/plugin/misc.lua
+++ config/nvim-new/plugin/misc.lua
@@ -1,3 +1,6 @@
+require("gitsigns").setup({})
+
+-- Oil
 require("oil").setup({
 	view_options = {
 		show_hidden = true,
